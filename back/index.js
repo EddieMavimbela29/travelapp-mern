1a53a046b51f7405d52be726b8db25320fce68c2
220647054 <220647054@ump.ac.za>
2022-02-12 10:09:48 +0200

    first commit

--- back/index.js
+++ back/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/pins", pinRoute);
 
-app.use(express.static(path.join(__dirname,"/client")));
+app.use(express.static(path.join(__dirname,"/client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname,"/client/build", "index.html"))
 })
